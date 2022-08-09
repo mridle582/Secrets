407ec5b36254c6510f420d68a6fa62c52e81eee6
mridle582 <mridle582@gmail.com>
2022-08-09 16:42:32 -0400

    create GET routes for login, register, and home

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static("public"));
-app.use('view-engine', 'ejs');
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
 
-
+app.get("/register", (req, res) => {
+    res.render("register");
+});
 
 
 app.listen(3000, ()=> {
